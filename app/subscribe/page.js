ade19971342286500983e0c5729bcd0f92f19513
Guney Unay <guneymarketing17@hotmail.com>
2024-01-04 21:25:58 +0300

    added subs container

---
+++ app/subscribe/page.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Subscribe() {
+  return <div></div>;
+}
+
+export default page;
---
+++ containers/SubscribeContainer/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function SubscribeContainer() {
+  return <main>subs container</main>;
+}
+
+export default SubscribeContainer;
