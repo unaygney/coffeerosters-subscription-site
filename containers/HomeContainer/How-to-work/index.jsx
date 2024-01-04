b094bb1fc7934210176df6b6683d5b40fbe5c4e9
Guney Unay <guneymarketing17@hotmail.com>
2024-01-04 17:26:37 +0300

    added minor style changes

--- containers/HomeContainer/How-to-work/index.jsx
+++ containers/HomeContainer/How-to-work/index.jsx
@@ -7,9 +7,9 @@ function Howtowork() {
   return (
     <section className="bg-transparent">
       <div className="container mx-auto">
-        <div className="flex flex-col gap-20 items-center md:items-start lg:px-[85px]">
-          <div className="flex md:flex-col w-full md:gap-10">
-            <h3 className="heading-s text-grey">How it works</h3>
+        <div className="flex flex-col gap-20 items-center md:items-start  lg:px-[85px]">
+          <div className="flex md:flex-col justify-center w-full md:gap-10">
+            <h3 className="heading-s text-grey ">How it works</h3>
             {/* Stick */}
             <div className="md:w-[75%] lg:w-[72%] h-[2px] bg-pale-orange hidden md:block relative">
               <span className="w-[31px] h-[31px] rounded-full border-2 border-dark-cyan bg-light-cream absolute top-1/2 -translate-y-1/2 left-0"></span>
